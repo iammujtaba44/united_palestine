3775b3cd115e0bbe8325e15b05b8097343699d47
Mujtaba <immujtaba44@gmail.com>
2020-11-27 00:25:55 +0500

    Second phase done 2

--- lib/view/drawer_screens/profile.dart
+++ lib/view/drawer_screens/profile.dart
@@ -2,7 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 import 'package:united_palestine/project_theme.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  bool isExpanded = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,45 +21,110 @@ class ProfileScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.only(top: 30, left: 20, right: 20),
         child: ListView.builder(
-          itemCount: titles.length,
-          itemBuilder: (context, index) => Card(
-              elevation: 2,
-              child: ExpansionTile(childrenPadding: EdgeInsets.only(top: 10,bottom: 10),
-                title: Text(titles[index]),
-                children: [
-                  Column(
-                    children: [
-                      BuildCards('Name:','Tamer'),
-                      BuildCards('Last Name:','Atia'),
-                      BuildCards('Date of Birth:','07/09/1980'),
-                      BuildCards('Gender:','Male'),
-                      BuildCards('City of Residence:','Amman, Jordon'),
-                      BuildCards('Mobile Number:','+962 79 5822348'),
-                    ],
-                  )
-                ],
-              )),
-        ),
+            itemCount: titles.length,
+            itemBuilder: (context, index) => Column(
+                  children: [
+                    Container(
+                      decoration:
+                          BoxDecoration(color: Colors.white, boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.2),
+                          spreadRadius: 1,
+                          blurRadius: 1,
+                          offset: Offset(1, 3),
+                        )
+                      ]),
+                      child: ExpansionTile(
+                        trailing: isExpanded
+                            ? Icon(
+                                Icons.keyboard_arrow_down,
+                                color: ProjectTheme.projectPrimaryColor,
+                              )
+                            : Icon(
+                                Icons.keyboard_arrow_right_outlined,
+                                color: ProjectTheme.projectPrimaryColor,
+                              ),
+                        onExpansionChanged: (value) {
+                          setState(() {
+                            if (isExpanded)
+                              isExpanded = false;
+                            else
+                              isExpanded = true;
+                          });
+                        },
+                        childrenPadding: EdgeInsets.only(top: 10, bottom: 10),
+                        title: Text(
+                          titles[index],
+                          style: TextStyle(
+                              color: ProjectTheme.projectPrimaryColor,
+                              fontSize: 18.5),
+                        ),
+                        children: [
+                          Column(
+                            children: [
+                              BuildCards('Name:', 'Tamer'),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              BuildCards('Last Name:', 'Atia'),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              BuildCards('Date of Birth:', '07/09/1980'),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              BuildCards('Gender:', 'Male'),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              BuildCards('City of Residence:', 'Amman, Jordon'),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              BuildCards('Mobile Number:', '+962 79 5822348'),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    )
+                  ],
+                )),
       ),
     );
   }
-  Widget BuildCards (String title, String description){
-    return Container(height: 55,
-      child: Card(
-          elevation: 2,
-          child: Padding(
-            padding: const EdgeInsets.only(left:0),
-            child: Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [Text(
-              title,textAlign: TextAlign.left,
-              style: TextStyle(fontSize: 12),
+
+  Widget BuildCards(String title, String description) {
+    return Container(
+      height: 55,
+      decoration: BoxDecoration(color: Colors.white, boxShadow: [
+        BoxShadow(
+          color: Colors.grey.withOpacity(0.2),
+          spreadRadius: 1,
+          blurRadius: 1,
+          offset: Offset(1, 3),
+        )
+      ]),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text(
+              title,
+              textAlign: TextAlign.left,
+              style: TextStyle(fontSize: 15.5, fontWeight: FontWeight.w400),
             ),
-                Text(
+            Text(
               description,
-              style: TextStyle(fontSize: 12),
-            ),],
+              style: TextStyle(fontSize: 15.5, fontWeight: FontWeight.w400),
             ),
-          )),
+          ],
+        ),
+      ),
     );
   }
 
