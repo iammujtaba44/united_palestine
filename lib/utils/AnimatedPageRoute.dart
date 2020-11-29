2f7c15136078af8ea15647d0285d3ab18a46ee33
Mujtaba <immujtaba44@gmail.com>
2020-11-29 20:46:09 +0500

    Transitions on routes done2

--- lib/utils/AnimatedPageRoute.dart
+++ lib/utils/AnimatedPageRoute.dart
@@ -12,7 +12,7 @@ class AnimatedPageRoute extends PageRouteBuilder {
               // animation = CurvedAnimation(
               //     parent: animation,
               //     curve: Curves.elasticInOut);
-              var begin = Offset(0.0, 1.0);
+              var begin = Offset(-1, 0);
               var end = Offset.zero;
               var curve = Curves.ease;
               // var tween = Tween(begin: begin, end: end);
